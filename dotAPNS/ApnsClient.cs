596164fdf875949e307e1221fb9dda58e6e39112
Alex Alok <contact@alexalok.me>
2020-01-14 14:03:40 +0300

    Fix certificate validity detection when running on non-Windows system

--- dotAPNS/ApnsClient.cs
+++ dotAPNS/ApnsClient.cs
@@ -59,7 +59,12 @@ namespace dotAPNS
             _http = http;
             var split = cert.Subject.Split(new[] { "0.9.2342.19200300.100.1.1=" }, StringSplitOptions.RemoveEmptyEntries);
             if (split.Length != 2)
-                throw new InvalidOperationException("Provided certificate does not appear to be a valid APNs certificate.");
+            {
+                // On Linux .NET Core cert.Subject prints `userId=xxx` instead of `0.9.2342.19200300.100.1.1=xxx`
+                split = cert.Subject.Split(new[] { "userId=" }, StringSplitOptions.RemoveEmptyEntries);
+                if (split.Length != 2)
+                    throw new InvalidOperationException("Provided certificate does not appear to be a valid APNs certificate.");
+            }
             string topic = split[1];
             _isVoipCert = topic.EndsWith(".voip");
             _bundleId = split[1].Replace(".voip", "");

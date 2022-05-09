7a2813f5030f4a2e2e90f31bb52657763a5f93b2
Henry <emorell96@hotmail.com>
2022-05-09 13:54:22 +0200

    made the default httpversion 2.0 in net5,6

--- dotAPNS.AspNetCore/AspNetCoreExtensions.cs
+++ dotAPNS.AspNetCore/AspNetCoreExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,12 +12,28 @@ namespace dotAPNS.AspNetCore
             var optionsBuilder = services.AddOptions<ApnsServiceOptions>();
             if (configureApns != null)
                 optionsBuilder.Configure(configureApns);
-            services.AddHttpClient("dotAPNS");
+            services.AddHttpClient("dotAPNS")
+#if NET5_0_OR_GREATER
+                .ConfigureHttpClient(client =>
+                {
+                    client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher;
+                    client.DefaultRequestVersion = HttpVersion.Version20;
+                })
+#endif
+                ;
             services.AddHttpClient("dotAPNS_DisableCerverCertValidation")
                 .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler()
                 {
                     ServerCertificateCustomValidationCallback = (m, x, c, s) => true
-                });
+                })
+#if NET5_0_OR_GREATER
+                .ConfigureHttpClient(client =>
+                {
+                    client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher;
+                    client.DefaultRequestVersion = HttpVersion.Version20;
+                })
+#endif
+                ;
             services.AddSingleton<IApnsClientFactory, ApnsClientFactory>();
             services.AddSingleton<IApnsService, ApnsService>();
             return services;
